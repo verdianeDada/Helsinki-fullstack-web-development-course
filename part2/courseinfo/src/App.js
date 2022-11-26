20f36c0998789ed2cdae5e6be4b8b4f32e7073fa
verdiandeDada <geraldine.dada@gmail.com>
2022-11-26 23:49:43 +0100

    Courseinfo of part2 completed

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>
-}
-const Content = (props) => {
-  const parts = props.course.parts
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </>
-  )
-}
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Total = (props) => {
-  const parts = props.course.parts
-  const total = parts.reduce((sum, part) => {
-    console.log(sum)
-    return sum + part.exercises
-  }, 0)
-
-  return <h4>Number of exercises {total}</h4>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>)
-}
+import Course from './components/Course'
 const App = () => {
   const courses = [
     {
