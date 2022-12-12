be62b61640b3412942c8dd498609b659dc9c8fd9
verdiandeDada <geraldine.dada@gmail.com>
2022-12-12 12:39:36 +0100

    CRUD operation works  succesfully"

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,56 +3,94 @@ import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import PersonLogic from './Logic'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('Ada Lovelace')
+  const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("39-44-5323523")
   const [filteredPersons, setFilteredPersons] = useState([])
   const [isFiltered, setIsFiltered] = useState(false)
 
-  useEffect (() => {
-    axios.get('http://localhost:3001/persons')
-    .then(response => {
-      const persons = response.data
-      setPersons(persons)
-    })
+  useEffect(() => {
+    PersonLogic
+      .getAll()
+      .then(persons => {
+        setPersons(persons)
+      })
+      .catch(error => console.log(error)
+      )
   }, [])
 
   const AddingName = (event) => {
     event.preventDefault()
-    const tempNewPerson = {
+    const tempPerson = {
       name: newName,
       number: newNumber
     }
 
 
     // checking if persons already has an object with the new name
-    persons.some(person => person.name === newName)
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(tempNewPerson))
+    const existPerson = persons.find(p => p.name === newName)
+    if (existPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with the new one?`)) {
+        PersonLogic
+          .update(existPerson.id, tempPerson)
+          .then(changedPerson => {
+            console.log('updated')
+            setPersons(persons.map(p => p.id === changedPerson.id ? changedPerson : p))
+            setNewName('')
+            setNewNumber("39-44-5323523")
+          })
+          .catch(error => console.log(error))
+      }
+    }
+    else {
+      PersonLogic
+        .create(tempPerson)
+        .then(newPerson => {
+          console.log('inserted')
+          setPersons(persons.concat(newPerson))
+          setNewName('')
+          setNewNumber("39-44-5323523")
+        })
+        .catch(error => console.log(error))
+    }
   }
+
+  const deletePerson = id => {
+    if (window.confirm("Do you really want to delete this name?")) {
+      PersonLogic
+        .delete(id)
+        .then(res => {
+          console.log("deleted")
+          setPersons(persons.filter(p => p.id !== id))
+        })
+        .catch(error => console.log(error))
+    }
+  }
+
   const Persons = () => {
     if (isFiltered)
       return (
         <div>
-          {filteredPersons.map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
+          {filteredPersons.map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
         </div>
       )
     else
       return (
         <div>
-          {persons.map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
+          {persons.map((person) => <p key={person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person.id)}>delete</button></p>)}
         </div>
       )
   }
 
   const filter = (event) => {
-    
+
     event.target.value
       ? setIsFiltered(true)
       : setIsFiltered(false)
-console.log(isFiltered);
+    console.log(isFiltered);
 
     if (isFiltered) {
       const tempFiltered = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
@@ -64,9 +102,9 @@ console.log(isFiltered);
   return (
     <div>
       <h2>Phonebook</h2>
-       <Filter onChange = {filter}/>
-        <h2>Add a new</h2>
-        <PersonForm AddingName = {AddingName} newName = {newName} setNewName = {setNewName} setNewNumber = {setNewNumber}  newNumber = {newNumber} />
+      <Filter onChange={filter} />
+      <h2>Add a new</h2>
+      <PersonForm AddingName={AddingName} newName={newName} setNewName={setNewName} setNewNumber={setNewNumber} newNumber={newNumber} />
       <h2>Numbers</h2>
       <Persons />
 
---
+++ part2/phonebook/src/Logic.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+const baseUrl = 'http://localhost:3001/persons'
+
+
+const create = NewPerson => {
+    const request = axios.post(baseUrl, NewPerson)
+    return request.then(res => res.data)
+}
+
+const getAll = () => {
+    const request = axios.get('http://localhost:3001/persons')
+    return request.then(res => res.data)
+}
+
+const update = (id, person) => {
+    const request = axios.put(`${baseUrl}/${id}`, person)
+    return request.then(res => res.data)
+}
+
+const deletePerson = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(res => res.data)
+}
+
+export default {
+    create,
+    delete: deletePerson,
+    getAll,
+    update
+}
